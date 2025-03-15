e7bf00c4af3f1ab571106a714ddb7895c990cd9e
IzaelDev <gabrielhizaguirre@gmail.com>
2025-03-14 22:53:35 -0400

    css changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,10 @@
 import Nav from "./Nav";
-import FadeInUp from "./FadeInUp";
 import FadeInLeft from "./FadeInLeft";
 import FadeInRight from "./FadeInRight";
-import Card from "./Card";
 import Button from "./Button";
 import Island from "./Island";
 import Makar from "./Makar";
+import Card from "./Card";
 import Wind from "./Wind";
 
 export {
